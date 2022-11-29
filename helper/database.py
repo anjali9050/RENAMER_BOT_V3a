e734ac78508312c4d30d56a35445fc2025b06deb
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-11-29 12:27:29 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,7 +1,7 @@
 import pymongo 
 import os
 from helper.date import add_date
-DB_NAME = os.environ.get("DB_NAME","anumitultrabots")
+DB_NAME = os.environ.get("DB_NAME","anumitultrabot")
 DB_URL = os.environ.get("DB_URL","mongodb+srv://amit:amit@cluster0.1je0krd.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
