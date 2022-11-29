36f5c8de005447ec6ddcf6d172c76bc85a74e371
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-11-29 12:31:32 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+apt update && apt upgrade -y
+apt install git -y
+pip3 install -U pip
+apt install git python3-pip ffmpeg -y
+
+git clone https://github.com/anumitultra/RENAMER_BOT_V3a.git          
+pip3 install -r requirements.txt
+
+python bot.py
