19511a595c6eb87a4be032496829d949c9eaa3d0
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-11-29 13:34:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,4 @@ apt install git python3-pip ffmpeg -y
 
 git clone https://github.com/anumitultra/RENAMER_BOT_V3a.git          
 pip3 install -r requirements.txt
-npm install http-server -g
-http-server -p $PORT &
 python bot.py
