6f2ecf014c87af1eff1e29aaa68a948e39e66865
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-11-30 10:01:09 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,7 +5,7 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 5GB
+	Daily  Upload limit 2GB
 	Price 0
 	
 	_____________________________
